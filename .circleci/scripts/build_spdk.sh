4fb394b43bd09ebd4ecbc1cfce53581a3e418f87
jkozlowski <mail@jakub-kozlowski.com>
2018-09-22 00:22:13 +0100

    Remove sudo

--- .circleci/scripts/build_spdk.sh
+++ .circleci/scripts/build_spdk.sh
@@ -6,7 +6,7 @@ git clone git@github.com:spdk/spdk.git
 cd /tmp/spdk
 git checkout v18.07.1
 git submodule update --init
-sudo ./scripts/pkgdep.sh
+./scripts/pkgdep.sh
 
 ./configure
-sudo make install
+make install
