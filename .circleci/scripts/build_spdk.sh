b0241543e402ecbe975f2cd62569e71ce2fbf1b2
jkozlowski <mail@jakub-kozlowski.com>
2018-09-22 00:31:48 +0100

    Try this

--- .circleci/scripts/build_spdk.sh
+++ .circleci/scripts/build_spdk.sh
@@ -6,6 +6,8 @@ git clone git@github.com:spdk/spdk.git
 cd /tmp/spdk
 git checkout v18.07.1
 git submodule update --init
+
+export DEBIAN_FRONTEND=noninteractive
 apt-get update
 ./scripts/pkgdep.sh
 
