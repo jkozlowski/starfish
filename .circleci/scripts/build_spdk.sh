c2a7667600848a7859be00844d4504908b30ceb2
jkozlowski <mail@jakub-kozlowski.com>
2018-09-22 21:24:20 +0100

    Try a different caching strategy

--- .circleci/scripts/build_spdk.sh
+++ .circleci/scripts/build_spdk.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
+cd /tmp
+git clone git@github.com:spdk/spdk.git
+
+cd /tmp/spdk
+git checkout v18.07.1
+git submodule update --init
+
+echo 'APT::Get::Assume-Yes "true"; APT::Get::force-yes "true";' > /etc/apt/apt.conf.d/99force-yes
+apt-get update
+./scripts/pkgdep.sh
+
 if [ ! -f "/usr/local/lib/libspdk.so" ]; then
-    cd /tmp
-    git clone git@github.com:spdk/spdk.git
-
-    cd /tmp/spdk
-    git checkout v18.07.1
-    git submodule update --init
-
-    echo 'APT::Get::Assume-Yes "true"; APT::Get::force-yes "true";' > /etc/apt/apt.conf.d/99force-yes
-    apt-get update
-    ./scripts/pkgdep.sh
-
     ./configure
     make install
 else
