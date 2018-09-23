d1905591b3bbc6049038244a3cc15fc133e67dfd
jkozlowski <mail@jakub-kozlowski.com>
2018-09-23 19:44:47 +0100

    Fix

---
+++ .circleci/scripts/run_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain none -y
+source $HOME/.cargo/env
+dd if=/dev/zero of=/tmp/aiofile bs=2048 count=5000
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/usr/local/lib"
+cd /home/circleci/project
+cargo test --all
