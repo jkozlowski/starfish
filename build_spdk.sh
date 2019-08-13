9c4a3296cb443cd9a609e5e8787394ce0086fbd9
jkozlowski <mail@jakub-kozlowski.com>
2019-08-13 12:29:24 +0100

    Add

--- build_spdk.sh
+++ build_spdk.sh
@@ -1,16 +1,17 @@
 #!/bin/bash -v
 
-git clone https://github.com/jkryl/spdk-sys.git
-
-cd /tmp/spdk-sys
-git submodule update --init --recursive
-
 apt-get update
 # TODO(jkozlowski) Put in custom docker container
 apt-get install -y \
     git mercurial xvfb apt \
     locales sudo openssh-client ca-certificates tar gzip parallel \
     net-tools netcat unzip zip bzip2 gnupg curl wget make
+
+git clone https://github.com/jkryl/spdk-sys.git
+
+cd /tmp/spdk-sys
+git submodule update --init --recursive
+
 # TODO(jkozlowski) Add this to spdk-sys
 ./spdk-sys/spdk/scripts/pkgdep.sh
 # TODO(jkozlowski) What is this for?
