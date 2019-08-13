f7adfe4ca507e2bcb7b70dadcbfdc96d739e8553
jkozlowski <mail@jakub-kozlowski.com>
2019-08-13 10:41:45 +0100

    Install libs

--- .circleci/scripts/build_spdk.sh
+++ .circleci/scripts/build_spdk.sh
@@ -3,6 +3,11 @@
 git submodule update --init --recursive
 
 sudo apt-get update
+# TODO(jkozlowski) Put in custom docker container
+sudo apt-get install -y \
+    git mercurial xvfb apt \
+    locales sudo openssh-client ca-certificates tar gzip parallel \
+    net-tools netcat unzip zip bzip2 gnupg curl wget make
 # TODO(jkozlowski) Add this to spdk-sys
 sudo ./spdk-sys/spdk/scripts/pkgdep.sh
 # TODO(jkozlowski) What is this for?
