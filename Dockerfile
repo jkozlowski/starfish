88ae7856cf9847bd4dfe48b39b1bfa2ac9f13cc9
jkozlowski <mail@jakub-kozlowski.com>
2019-08-13 12:26:26 +0100

    Custom docker image

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM ubuntu:eoan-20190717.1
+
+ ADD build_spdk.sh /project/build_spdk.sh
+
+ RUN sh /project/build_spdk.sh
+
+ RUN curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain none -y
+
+ ADD . /project
+
+ RUN cd /project && cargo fetch
--- build_spdk.sh
+++ build_spdk.sh
@@ -1,25 +1,25 @@
 #!/bin/bash -v
 
+git clone https://github.com/jkryl/spdk-sys.git
+
+cd /tmp/spdk-sys
 git submodule update --init --recursive
 
-sudo apt-get update
+apt-get update
 # TODO(jkozlowski) Put in custom docker container
-sudo apt-get install -y \
+apt-get install -y \
     git mercurial xvfb apt \
     locales sudo openssh-client ca-certificates tar gzip parallel \
     net-tools netcat unzip zip bzip2 gnupg curl wget make
 # TODO(jkozlowski) Add this to spdk-sys
-sudo ./spdk-sys/spdk/scripts/pkgdep.sh
+./spdk-sys/spdk/scripts/pkgdep.sh
 # TODO(jkozlowski) What is this for?
-sudo apt-get install -y module-init-tools
+apt-get install -y module-init-tools
 
 sh ./spdk-sys/build.sh
+cp spdk-sys/build/libspdk_fat.so /usr/local/lib
 
-sudo ./spdk-sys/spdk/scripts/setup.sh
+./spdk-sys/spdk/scripts/setup.sh
 
-if [ ! -f "/usr/local/lib/libspdk.so" ]; then
-    cd spdk-sys/spdk
-    sudo make install
-else
-    echo "spdk already built"
-fi
+cd spdk-sys/spdk
+make install
